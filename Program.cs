3793fdc258b2d83236891b4a69d7361f0a6282a8
Alejandro-Ogando <aogando09@gmail.com>
2020-08-12 03:01:10 -0400

    final update

--- Program.cs
+++ Program.cs
@@ -18,6 +18,7 @@ namespace namePicker
         //Ian R. De los Santos. ID: 1095250
 
         //this method works with the random list
+
         static List<string> RandomList(List<string> array)
         {
             Random random = new Random();
@@ -35,16 +36,16 @@ namespace namePicker
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Introduzca la ruta del archivo txt, que contenga los estudiantes:");
-            string filePath = Console.ReadLine();
-            Console.WriteLine("Introduzca la ruta del archivo txt, que contenga los grupos:");
-            string filePath1 = Console.ReadLine();
-            Console.WriteLine("Introduzca la ruta del archivo txt, que contenga los temas:");
-            string filePath2 = Console.ReadLine();
+            Console.Write("Introducirla ruta del archivo txt, que contenga los estudiantes: ");
+            string filepath = Console.ReadLine();
+            Console.WriteLine("Introduzca la ruta del archivo txt, que contenga los grupos: ");
+            string filepath1 = Console.ReadLine();
+            Console.WriteLine("Introduzca la ruta del archivo txt, que contenga los temas: ");
+            string filepath2 = Console.ReadLine();
 
-            List<string> students = File.ReadAllLines(filePath).ToList();
-            List<string> groups = File.ReadAllLines(filePath1).ToList();
-            List<string> topics = File.ReadAllLines(filePath2).ToList();
+            List<string> students = File.ReadAllLines(filepath).ToList();
+            List<string> groups = File.ReadAllLines(filepath1).ToList();
+            List<string> topics = File.ReadAllLines(filepath2).ToList();
 
             students = RandomList(students);
             groups = RandomList(groups);
@@ -53,33 +54,64 @@ namespace namePicker
             float studentsAmount = students.Count() / groups.Count();
             float topicsAmount = topics.Count() / groups.Count();
 
-            for(int i = 0; i < groups.Count(); i++)
+            // Make an array for extras and make sure that no element is left out
+            int[] students1 = new int[groups.Count()]; // {null,null,null,null,...null}
+            for (int i = 0; i < students1.Length; i++)
             {
-                Console.WriteLine("------------------------------");
-                Console.WriteLine($"\n{groups[i]}");
-                Console.WriteLine("------------------------------");
+                students1[i] = 0; // {0,0,0,0,...0}
+            }
 
-                Console.WriteLine("Integrantes: " + studentsAmount);
+            int[] topics1 = new int[groups.Count()];
+            for (int i = 0; i < topics1.Length; i++)
+            {
+                topics1[i] = 0;
+            }
+
+            // Add extras to arrays
+            int b = 0;
+            while (((students.Count() - b) % groups.Count()) > 0)
+            {
+                students1[b] = 1;
+                b++;
+            }
+
+            int c = 0;
+            while (((topics.Count() - c) % groups.Count()) > 0)
+            {
+                topics1[c] = 1;
+                c++;
+            }
+
+            // Print
+            for (int i = 0; i < groups.Count(); i++)
+            {
+                Console.WriteLine("----------------");
+                Console.WriteLine($"\n{groups[i]}");
+                Console.WriteLine("----------------");
+
+                Console.WriteLine("Integrantes:" + (studentsAmount + students1[i]));
                 int e = 0;
-                while (e < studentsAmount)
+
+                while (e < (studentsAmount + students1[i]))
                 {
                     Console.WriteLine(students[0]);
                     students.RemoveAt(0);
                     e++;
+
                 }
 
-               Console.WriteLine("Cantidad de Temas: " + topicsAmount);
+                Console.WriteLine("Cantidad de temas:" + (topicsAmount + topics1[i]));
+
                 int t = 0;
-                while(t < topicsAmount)
+
+                while (t < (topicsAmount + topics1[i]))
                 {
                     Console.WriteLine(topics[0]);
                     topics.RemoveAt(0);
                     t++;
                 }
             }
-
-            Console.ReadLine();
+            Console.ReadKey();
         }
-
     }
 }
